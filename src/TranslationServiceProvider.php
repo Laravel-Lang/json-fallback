ff9e51e0d1c9d6080620790c89d110bc04c1a282
Andrey Helldar <helldar@ai-rus.com>
2022-06-29 12:41:47 +0300

    Added provider with service

---
+++ src/TranslationServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaravelLang\JsonFallbackHotfix;
+
+use Illuminate\Translation\TranslationServiceProvider as BaseServiceProvider;
+
+class TranslationServiceProvider extends BaseServiceProvider
+{
+    public function register()
+    {
+        $this->registerLoader();
+
+        $this->app->singleton('translator', function ($app) {
+            $loader = $app['translation.loader'];
+
+            $locale = $app['config']['app.locale'];
+
+            $trans = new Translator($loader, $locale);
+
+            $trans->setFallback($app['config']['app.fallback_locale']);
+
+            return $trans;
+        });
+    }
+}
