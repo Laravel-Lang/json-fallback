2cf8c89c80665f6a0fa40d7c0c9d3067d0d41a2a
Andrey Helldar <helldar@dragon-code.pro>
2024-03-13 12:15:08 +0300

    Added Laravel 11 support

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of the "dragon-code/json-fallback" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ * @copyright 2024 Andrey Helldar
+ * @license MIT
+ *
+ * @see https://github.com/TheDragonCode/json-fallback
+ */
+
 declare(strict_types=1);
 
 namespace LaravelLang\JsonFallback;
